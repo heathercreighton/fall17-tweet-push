5468b00dfdc5cb392e042e5e92d7620bb7adccbc
Heather Creighton <heather.creighton@gmail.com>
2017-10-06 09:39:00 -0400

    added code to account for no tweets

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,22 +11,27 @@ module ApplicationHelper
     end 
 
     return count
-   end 
+  end 
 
 
-   def tweet_count(user_id)
+  def tweet_count(user_id)
 
    	tweets = []
    	tweets = Tweet.where(user_id: user_id)
 
    	return tweets.length
 
-   end	
+  end	
 
-   def top_tweeter
-    user_id =  Tweet.all.group_by { |h| h['user_id']}.to_a.max_by {|x| x[1].length}.first 
-    user = User.find(user_id)
-    return user
-   end
+  def top_tweeter
+    tweets = Tweet.all
+    if tweets.length > 0
+      user_id =  Tweet.all.group_by { |h| h['user_id']}.to_a.max_by {|x| x[1].length}.first 
+      user = User.find(user_id)
+      return user
+    end
+
+        
+  end
 
 end
